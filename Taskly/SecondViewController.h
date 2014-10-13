7b9c11dc52f5b5e72135451bcf1811ff0d96ead6
Dovo <justice287@gmail.com>
2014-10-12 23:40:09 -0700

    Initial Commit

---
+++ Taskly/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Taskly
+//
+//  Created by Miles Laff on 10/12/14.
+//  Copyright (c) 2014 ASU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
