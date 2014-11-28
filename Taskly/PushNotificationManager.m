cac2323eb48d1cfef4398e2d89ea32b5448ae000
Dovo <justice287@gmail.com>
2014-11-27 20:44:38 -0700

    Wrote function to send updates to offering users when task updated

--- Taskly/PushNotificationManager.m
+++ Taskly/PushNotificationManager.m
@@ -10,17 +10,24 @@
 
 @implementation PushNotificationManager
 
+// when an offer is saved, send notification to task owner, telling them they have a new offer
 +(void)sendOfferNotification:(PFObject *) offer {
     PFQuery *pushQuery = [PFQuery queryWithClassName:@"Tasks"];
     PFUser *taskOwner = [[offer objectForKey:@"forTask"] objectForKey:@"owner"];
-    
     [pushQuery whereKey:@"owner" equalTo:taskOwner];
     
     [PFPush sendPushMessageToQueryInBackground:pushQuery withMessage:@"You have a new offer! Please check your Account page to view it."];
 }
 
+// when a task is saved, send push notifications to people who offered on it
 +(void)sendResponseNotification:(PFObject *) task {
-    //ill do this later
+    PFQuery *pushQuery = [PFQuery queryWithClassName:@"Offers"];
+    [pushQuery whereKey:@"forTask" equalTo:task];
+    
+    // we could use this later on if we want to send different messages to accepted/rejected offers
+    //[pushQuery whereKey:@"user" notEqualTo:[task objectForKey:@"filler"];
+     
+    [PFPush sendPushMessageToQueryInBackground:pushQuery withMessage:@"Once of the tasks you've made an offer on has been updated. Please check your Account page to see the updates."];
 }
 
 @end
