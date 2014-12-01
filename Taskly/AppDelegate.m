8770e92b6170106bbd102e9aab0287d5f9ff3921
Benson Truong <btruong1@asu.edu>
2014-12-01 12:33:15 -0700

    Removed some Tasks in AppDelegate Fixed layout

--- Taskly/AppDelegate.m
+++ Taskly/AppDelegate.m
@@ -7,15 +7,10 @@
 //
 
 // Below are nice features to add
-#warning TODO: implement more Facebook stuff, get profile picture, email, phone number to use somewhere
-#warning TODO: fill out email field when responding to task automaticlaly with email stored on Parse
 #warning TODO: allow users to search by task given location
-#warning TODO: create negotiation process, ask task originator for said price
 #warning TODO: use previous completed task history to give user tasks from the same person
 #warning TODO: create a rating mechanism for trusted task do-ers, increase as more tasks are marked complete and task originator has marked success/good job done
 #warning TODO: Allow users to use Phone Number to login, instead of Facebook
-#warning TODO: Allow Points of Interest search on maps, currently only citys are searched, example: Safeway, Frys
-#warning TODO: Push notifications to users/filler when job has been accepted/completed
 
 #import "AppDelegate.h"
 #import <Parse/Parse.h>
